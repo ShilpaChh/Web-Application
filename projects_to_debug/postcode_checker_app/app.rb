a24b941139a3713455eb6d0b1af6004a69b59dd2
SHILPA CHHABRA <chhabrashilpa@yahoo.com>
2023-03-31 11:19:39 +0100

    Web App - Phase 3 - Ch5 - Debugging postcode_checker

--- projects_to_debug/postcode_checker_app/app.rb
+++ projects_to_debug/postcode_checker_app/app.rb
@@ -12,8 +12,11 @@ class Application < Sinatra::Base
     return erb(:index)
   end
 
-  get '/check' do
-    valid = PostcodeChecker.new.valid?(params[:postcode])
+  post '/check' do
+    # valid = PostcodeChecker.new.valid?(params[:postcode])
+    @valid = PostcodeChecker.new.valid?(params[:postcode])
+
+
     return erb(:check)
   end
 end
--- projects_to_debug/postcode_checker_app/lib/postcode_checker.rb
+++ projects_to_debug/postcode_checker_app/lib/postcode_checker.rb
@@ -1,6 +1,9 @@
 class PostcodeChecker
   def valid?(postcode)
     if postcode.match(/^[a-z]{1,2}\d[a-z\d]?\s*\d[a-z]{2}$/i)
+        # Reference: https://regex101.com/
+        # type this regex in the textbox and hover each to see what it does..
+
       return true
     else
       return false
